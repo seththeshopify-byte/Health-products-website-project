ee0d023f774167d8c6a0ddcf0843e06f06181cc9
seththeshopify-byte <seth.theshopify@gmail.com>
2026-09-02 14:54:55 +0300

    Adjust logo size in Home component

--- artifacts/ruth-health/src/pages/Home.tsx
+++ artifacts/ruth-health/src/pages/Home.tsx
@@ -270,7 +270,7 @@ export default function Home() {
               <img
                 src="/attached_assets/generated_images/RuthHotelLogo.png"
                 alt="Benington Hotel & Suite"
-                className="h-24 md:h-28 w-auto object-contain"
+                className="h-16 sm:h-20 md:h-28 w-auto object-contain"
               />
 
               <div>
